dcc3f14c764a2b9763a3be27ab6c6b48464caca2
protap <protap@kstych.com>
2024-09-21 22:36:18 +0530

    sidebar

--- src/Components/Blog/Header.js
+++ src/Components/Blog/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-      <div className="my-6 text-center">
-      <h1>Blog-Mern</h1>
+    <div className="my-6">
+      <h1 className='text-center'>Blog-Mern</h1>
     </div>
   )
 }
